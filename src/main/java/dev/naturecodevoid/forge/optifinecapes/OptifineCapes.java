3b21c44e36dbc708f4a5963c739c0fdc4784c705
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-10-18 13:48:55 -0700

    Update OptifineCapes.java

--- src/main/java/dev/naturecodevoid/forge/optifinecapes/OptifineCapes.java
+++ src/main/java/dev/naturecodevoid/forge/optifinecapes/OptifineCapes.java
@@ -22,7 +22,7 @@ public class OptifineCapes {
     public static final String MODID = "optifinecapes";
     public static final String NAME = "Optifine Capes";
     public static final String VERSION = "1.0";
-    public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
+    public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.16.1]";
 
     public static Logger logger;
 
