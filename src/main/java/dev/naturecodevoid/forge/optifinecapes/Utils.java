6d547c31e90c19f14d0642d51e54c59eb1eb5ba1
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-10-19 07:50:20 -0700

    Update Utils.java

--- src/main/java/dev/naturecodevoid/forge/optifinecapes/Utils.java
+++ src/main/java/dev/naturecodevoid/forge/optifinecapes/Utils.java
@@ -13,12 +13,20 @@ public class Utils {
     private static final ResourceLocation noCapeImage = new ResourceLocation(OptifineCapes.MODID, "textures/no_cape.png");
 
     public static ThreadDownloadImageData getDownloadImageCape(ResourceLocation resourceLocation, String username) {
-        TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
+        TextureManager textureManager = getMinecraft().getTextureManager();
         ITextureObject iTextureObject = textureManager.getTexture(resourceLocation);
 
         iTextureObject = new ThreadDownloadImageData((File) null, String.format(Utils.optifineCapesUrl, username), Utils.noCapeImage, new ParseCape());
         textureManager.loadTexture(resourceLocation, iTextureObject);
 
         return (ThreadDownloadImageData) iTextureObject;
+    }
+
+    public static Minecraft getMinecraft() {
+        try {
+            return Minecraft.getMinecraft();
+        } catch (Exception ignored) {
+            return Minecraft.getInstance();
+        }
     }
 }
